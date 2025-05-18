d0540fdcc46e282f020eb5d02aafed2d0e239ece
Martina Pauer <martinapauer38@gmail.com>
2025-05-17 21:42:26 -0300

    Actualizar cirquex-test.py
    nomenclatura mas intuitiva y una corrección en la determinación de precio

--- cirquex-test.py
+++ cirquex-test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from cirquex import suscribir
+
 import time
 # Prueba para clase suscribir
 usuario = suscribir()
@@ -13,34 +14,46 @@ print(f'\nsuscripcion\n\n\tPrecio: {usuario.precio.__str__()}\n\tUsuario: {usuar
 personas = [usuario]
 
 for agregado in range(1, 5):
-    objeto = suscribir()
-    objeto.usuarios = 4
-<<<<<<< HEAD
-    objeto.precio = (objeto.precio / objeto.usuarios)
-=======
->>>>>>> 5b33f4d (Sigue mejorando)
-    personas.append(objeto)
+
+    suscripto = suscribir()
+
+    suscripto.usuarios = 4
+
+    suscripto.precio = (usuario.precio / suscripto.usuarios)
+
+    personas.append(suscripto)
 
 from cirquex import circuito_financiero
 
-def mostrar_circuito(objeto : circuito_financiero):
-    if objeto.ingreso >= 0:
-        print(f'\tEl costo es de ${objeto.costo.__str__()} y el ingreso de ${objeto.ingreso.__str__()}\n\nTotal: {objeto.suscriptos[0].usuarios.__str__()} usuarios.\n')
+def mostrar_circuito(circuito_mostrado : circuito_financiero):
+
+    if circuito_mostrado.ingreso >= 0:
+
+        print(f'\tEl costo es de ${circuito_mostrado.costo.__str__()} y el ingreso de ${circuito_mostrado.ingreso.__str__()}\n\nTotal: {circuito_mostrado.suscriptos[0].usuarios.__str__()} usuarios.\n')
     else:
-        print(f'Hay perdidas a precio ${objeto.suscriptos[0].precio} con {objeto.suscriptos[0].usuarios} usuarios.')    
+        print(f'Hay perdidas a precio ${circuito_mostrado.suscriptos[0].precio} con {circuito_mostrado.suscriptos[0].usuarios} usuarios.')    
 
 caminos = circuito_financiero()
+
 caminos.costo = 5
 
 segundos = 3
-for objeto in personas:
-    caminos.agregar_usuario(objeto)
-    print(f'\nsuscripcion\n\n\tPrecio: {objeto.precio.__str__()}.\n')
+
+for persona in personas:
+
+    caminos.agregar_usuario(persona)
+
+    print(f'\nsuscripcion\n\n\tPrecio: {persona.precio.__str__()}.\n')
+
     mostrar_circuito(caminos)
+
     time.sleep(segundos)
+
 caminos.quitar_usuario(usuario)
+
 for objeto in caminos.suscriptos:
-    print(f'\nsuscripcion\n\n\tPrecio: {objeto.precio.__str__()}\n')
+
+    print(f'\nsuscripcion\n\n\tPrecio: {persona.precio.__str__()}\n')
     mostrar_circuito(caminos)
     time.sleep(segundos)
 
