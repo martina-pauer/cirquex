ac88d6b48e0a4755633eed7ac9871158e8014b6a
Martina Pauer <martinapauer38@gmail.com>
2025-05-30 13:24:23 +0000

    Run test-18: all fixed

--- view.py
+++ view.py
@@ -34,5 +34,5 @@ save = open('cont.txt', 'w')
 save.write(f'{test_number.__str__()}')
 save.close()
 del save, users, user1, user2, user3, user4, cirq, test_number
-os.system('rm -R __pyacache__')
+os.system('rm -R __pycache__')
 
